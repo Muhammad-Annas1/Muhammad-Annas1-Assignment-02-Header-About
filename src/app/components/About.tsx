3db73d2d9f190725f984a8101f89734949ceff31
Muhammad Annas <ma2259050@gmail.com>
2024-10-02 15:24:14 +0500

    first commit

---
+++ src/app/components/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="p-1">
+      <h2 className= "text-2xl font-semibold">About Us</h2>
+      <p>
+        We are a team of developers passionate about building modern web applications with cutting-edge technologies like Next.js.
+      </p>
+    </section>
+  );
+};
+
+export default About;
